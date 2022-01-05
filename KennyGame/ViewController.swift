abdd70c5414d74588dedfaaabb08243138b732d1
emircan saglam <vsaglam@hotmail.com>
2022-01-05 22:00:09 +0300

    Initial Commit

---
+++ KennyGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KennyGame
+//
+//  Created by Emircan saglam on 5.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
